bc82f5b9380f835baf669a57af35d5caa2857363
scott mcwee <s.mcwee@hotmail.co.uk>
2015-04-23 21:54:21 +0100

    database file updates
    files

--- include/database.php
+++ include/database.php
@@ -1,8 +1,11 @@
 <?php
-$host = "localhost";
-$db_name = "honours";
-$username = "root";
-$password = "";
+error_reporting(E_ALL ^ E_DEPRECATED);
+?>
+<?php
+$host = "mysql.hostinger.co.uk";
+$db_name = "u272711457_hpsm";
+$username = "u272711457_hpsm";
+$password = "Strathaven5";
   
 try {
     $connection = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
@@ -14,10 +17,10 @@ catch(PDOException $exception){
 }
 ?>
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'honours');
+define('DB_SERVER', 'mysql.hostinger.co.uk');
+define('DB_USERNAME', 'u272711457_hpsm');
+define('DB_PASSWORD', 'Strathaven5');
+define('DB_DATABASE', 'u272711457_hpsm');
 class DB_Class {
     function __construct() {
         $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die('There has been an issue connecting to the database -> ' . mysql_error());
--- included/db_con.php
+++ included/db_con.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysql_connect("localhost","root","");
-mysql_select_db("honours",$con) or die(mysql_error($con));
+$con=mysql_connect("mysql.hostinger.co.uk","u272711457_hpsm","Strathaven5");
+mysql_select_db("u272711457_hpsm",$con) or die(mysql_error($con));
 error_reporting(E_ALL ^ E_NOTICE);
 ?>
